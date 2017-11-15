657015ea01122ec98a3e8d88dfb30471a3f806f4
Peter Dave Hello <hsu@peterdavehello.org>
2017-11-16 02:16:01 +0800

    Move licenses/concat.py to root with minor adjustments

--- concat.py
+++ concat.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python2
 import glob
 
-for name in glob.glob("*.txt"):
+for name in glob.glob("licenses/*.txt"):
     txt = open(name).read()
     print '<pre class="license" id="%s">' % name[:-4]
     print txt
