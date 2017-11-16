b1441a322b8f180f79b12573956d7fc71ea7a0fb
Peter Dave Hello <hsu@peterdavehello.org>
2017-11-17 01:56:47 +0800

    Update license filenames, remove .txt ext

--- concat.py
+++ concat.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2
 import glob
 
-for name in glob.glob("licenses/*.txt"):
+for name in glob.glob("licenses/*"):
     txt = open(name).read()
-    print '<pre class="license" id="%s">' % name[9:-4]
+    print '<pre class="license" id="%s">' % name[9:]
     print txt
     print '</pre>'
