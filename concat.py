c6aeac073570b3917940af45327adb755518d83e
Peter Dave Hello <hsu@peterdavehello.org>
2017-11-17 02:03:40 +0800

    Refactor the process to generate index.html

--- concat.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-import glob
-
-for name in glob.glob("licenses/*"):
-    txt = open(name).read()
-    print '<pre class="license" id="%s">' % name[9:]
-    print txt
-    print '</pre>'
---
+++ generator.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+temp="$(mktemp)"
+
+for license in $(ls licenses/*)
+do
+    {
+        echo "<pre class=\"license\" id=\"${license/licenses\//}\">"
+        cat "${license}"
+        echo "</pre>"
+    } >> "${temp}"
+done
+
+sed "/The License texts -->/r${temp}" index.src.html > index.html
+
